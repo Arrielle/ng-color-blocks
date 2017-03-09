29a9d74d7cdd923fb9e0972103f3c153c5dd6c8e
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-08 19:41:53 -0600

    CAN ADD NEW COLORS

--- server/public/scripts/controllers/settings.controller.js
+++ server/public/scripts/controllers/settings.controller.js
@@ -1,27 +1,15 @@
-colorBlocks.controller('SettingsController', ['DataFactory', function(DataFactory, $scope) {
+colorBlocks.controller('SettingsController', ['DataFactory', '$scope', function(DataFactory, $scope) {
   console.log("settings controller running");
   var self = this;
   self.specialMessage = 'This site is amazing!';
   self.colors = DataFactory.factoryColors;
   self.newColor = '';
-  self.colorObject = {};
-  // $scope.colors = self.colors;
-  // $scope.scopeColor = ['blue'];
-  // convertArrayToObject(self.colors);
+  self.newColorObject = {};
 
   self.addColor = function(){
     console.log('button was clicked');
-    self.colors.push(angular.copy(self.newColor));
+    self.colors.push({name: angular.copy(self.newColor)});
     console.log(self.colors)
-
   }
-
-  // function convertArrayToObject(colorArray){
-  //   for (var i = 0; i < colorArray.length; i++) {
-  //     var newColorObject = {
-  //       name: colorArray[i]
-  //   };
-  //   console.log(newColorObject);
-  // }
 
 }]);
--- server/public/scripts/factories/data.factory.js
+++ server/public/scripts/factories/data.factory.js
@@ -1,6 +1,9 @@
 colorBlocks.factory('DataFactory', [function() {
   console.log("data factory running");
-  var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
+  // var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
+
+  var colors = [{name:'red'}, {name: 'blue'}, {name: 'magenta'}, {name: 'green'}, {name: 'pink'}];
+
 
   return {
     factoryColors: colors
