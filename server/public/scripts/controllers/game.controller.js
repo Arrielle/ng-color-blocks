05b58f6f65d44f4720e1476b6ddb7bf09c115cfe
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-08 18:09:17 -0600

    can add new colors

--- server/public/scripts/controllers/game.controller.js
+++ server/public/scripts/controllers/game.controller.js
@@ -30,3 +30,5 @@ function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 }]);
+
+//SETTINGS FUNCTIONS
--- server/public/scripts/controllers/settings.controller.js
+++ server/public/scripts/controllers/settings.controller.js
@@ -1,6 +1,17 @@
-colorBlocks.controller('SettingsController', ['DataFactory', function($scope, DataFactory) {
+colorBlocks.controller('SettingsController', ['DataFactory', function(DataFactory, $scope) {
   console.log("settings controller running");
   var self = this;
   self.specialMessage = 'This site is amazing!';
+  self.colors = DataFactory.factoryColors;
+
+  self.newColor = '';
+
+
+  self.addColor = function(){
+    console.log('button was clicked');
+    self.colors.push(angular.copy(self.newColor));
+    console.log(self.colors)
+
+  }
 
 }]);
