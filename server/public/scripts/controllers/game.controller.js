47f2482896e051a4c65d4faa24c4c00533423aac
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-09 08:47:30 -0600

    changed order of high scores

--- server/public/scripts/controllers/game.controller.js
+++ server/public/scripts/controllers/game.controller.js
@@ -8,7 +8,7 @@ self.colors = DataFactory.factoryColors;
 //gets the array of player objects from the data factory
 self.players = DataFactory.factoryPlayers;
 //finds the most recent high score (should be the highest score)
-self.score = self.players[self.players.length - 1].highscore;
+self.score = self.players[0].highscore;
 //sets a new player to an empty string
 self.newPlayer = '';
 //sets a new highscore to false (to hide and unhide things in html)
@@ -50,7 +50,7 @@ self.handleInput = function(clickedColor) {
 //pushes new high score to the leaderboards when Add to Leaderboards button has been clicked
 self.newScore = function(){
   console.log('button was clicked');
-  self.players.push({name: angular.copy(self.newPlayer), highscore: self.currentScore})
+  self.players.unshift({name: angular.copy(self.newPlayer), highscore: self.currentScore})
   console.log('self.players', self.players);
   init();
 }
--- server/public/scripts/factories/data.factory.js
+++ server/public/scripts/factories/data.factory.js
@@ -2,8 +2,8 @@ colorBlocks.factory('DataFactory', [function() {
   console.log("data factory running");
   // var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
 
-  var colors = [{name:'red'}, {name: 'blue'}, {name: 'magenta'}, {name: 'green'}, {name: 'pink'}];
-  var players = [{name: 'Jen', highscore: 4}, {name:'Arrielle', highscore: 9}];
+  var colors = [{name:'red'}, {name: 'hidden'}, {name: 'magenta'}, {name: 'green'}, {name: 'pink'}];
+  var players = [{name:'Arrielle', highscore: 9}, {name: 'Jen', highscore: 4}];
 
   return {
     factoryColors: colors,
